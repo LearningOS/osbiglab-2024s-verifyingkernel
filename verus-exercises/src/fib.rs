e70c04ab4b2a45dad522947fdabda4bb16d9c677
Yufan You <youyf21@mails.tsinghua.edu.cn>
2024-04-07 10:36:36 +0800

    feat(fib): support up to fib(92)

--- verus-exercises/src/fib.rs
+++ verus-exercises/src/fib.rs
@@ -25,39 +25,66 @@ proof fn fib_mono(x: nat, y: nat)
     }
 }
 
-fn calc_fib(n: u32) -> (f: u32)
+const FIB_U64_MAX_N: u64 = 92;
+
+proof fn lemma_fib_maxn_fits_u64()
+    ensures
+        fib(FIB_U64_MAX_N as nat) <= u64::MAX,
+{
+    reveal_with_fuel(fib, 4);
+    assert(fib(5) < fib(10));
+    assert(fib(15) < fib(20));
+    assert(fib(25) < fib(30));
+    assert(fib(35) < fib(40));
+    assert(fib(45) < fib(50));
+    assert(fib(55) < fib(60));
+    assert(fib(65) < fib(70));
+    assert(fib(75) < fib(80));
+    assert(fib(85) < fib(90));
+
+    // shorter but slower:
+    // reveal_with_fuel(fib, 11);
+    // assert(fib(20) < fib(40));
+    // assert(fib(60) < fib(80));
+
+    assert(fib(FIB_U64_MAX_N as nat + 1) > u64::MAX);
+}
+
+fn calc_fib(n: u64) -> (f: u64)
     requires
-        n <= 20,
+        n <= FIB_U64_MAX_N,
     ensures
         f == fib(n as nat),
 {
     if n <= 1 {
         return 1;
     }
+
     let mut a = 1;
     let mut b = 1;
     let mut index = 0;
-    assert(fib(n as nat) <= u32::MAX) by { reveal_with_fuel(fib, 11) };
+
     while index < n - 1
         invariant
+            index < n <= FIB_U64_MAX_N,
             a == fib(index as nat),
             b == fib((index + 1) as nat),
-            index <= n - 1,
-            fib(n as nat) <= u32::MAX,
     {
-        assert((a + b) as nat <= u32::MAX) by {
-            fib_mono((index + 2) as nat, n as nat);
+        assert((a + b) as nat <= u64::MAX) by {
+            lemma_fib_maxn_fits_u64();
+            fib_mono((index + 2) as nat, FIB_U64_MAX_N as nat);
         }
         let t = a + b;
         a = b;
         b = t;
         index += 1;
     }
+
     b
 }
 
 fn test() {
-    for i in 0..20 {
+    for i in 0..93 {
         calc_fib(i);
     }
 }
@@ -65,7 +92,7 @@ fn test() {
 } // verus!
 
 fn main() {
-    for i in 0..20 {
+    for i in 0..=FIB_U64_MAX_N {
         println!("{i}: {}", calc_fib(i));
     }
 }
